92663238c9222f19208d07276f7f13b659715994
Gabe <gabeWilson42@gmail.com>
2018-09-28 23:33:50 -0500

    Initalizing git repository

---
+++ src/main/java/com/foreflight/airportinfoapi/airportinfoapi/AirportinfoapiApplication.java
@@ -0,0 +1,12 @@
+package com.foreflight.airportinfoapi.airportinfoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportinfoapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportinfoapiApplication.class, args);
+    }
+}
