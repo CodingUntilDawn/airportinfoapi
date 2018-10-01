3e3cedfa37188646e1c068e6ce44d0d4da5e7a78
Gabe <gabeWilson42@gmail.com>
2018-09-30 20:07:31 -0500

    Changing the endpoints to return ResponseEntities

--- src/main/java/com/foreflight/airportinfoapi/controllers/AirportWeatherController.java
+++ src/main/java/com/foreflight/airportinfoapi/controllers/AirportWeatherController.java
@@ -3,7 +3,9 @@ package com.foreflight.airportinfoapi.controllers;
 import com.foreflight.airportinfoapi.models.airportWeather.AirportWeatherModel;
 import com.foreflight.airportinfoapi.services.face.AirportWeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,12 +18,12 @@ public class AirportWeatherController {
     AirportWeatherService airportWeatherService;
 
     @RequestMapping(value = "/getAirportWeather/{identifier}", method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
-    public AirportWeatherModel getAirportWeather(@PathVariable("identifier") String identifier){
-        return airportWeatherService.getAirportWeatherByAirport(identifier);
+    public ResponseEntity<AirportWeatherModel> getAirportWeather(@PathVariable("identifier") String identifier){
+        return new ResponseEntity<>(airportWeatherService.getAirportWeatherByAirport(identifier), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/getMultipleAirportsWeather", method = RequestMethod.POST, produces= MediaType.APPLICATION_JSON_VALUE)
-    public List<AirportWeatherModel> getMultipleAirportsWeather(@RequestBody List<String> identifiers){
-        return airportWeatherService.getAirportWeathersByAirports(identifiers);
+    public ResponseEntity<List<AirportWeatherModel>> getMultipleAirportsWeather(@RequestBody List<String> identifiers){
+        return new ResponseEntity<>(airportWeatherService.getAirportWeathersByAirports(identifiers), HttpStatus.OK);
     }
 }
